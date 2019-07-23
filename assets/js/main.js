971c8d30ca742b7105d3d4c056fdf1f1dc7a59bd
Victor Nguyen (victong2) <victong2@cisco.com>
2019-07-24 01:45:55 +0200

    countdown with hours

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,21 @@ function myAge() {
 }
 
 function countDownCanada() {
-    var departure = new Date('August 11, 2019 12:00:00');
-    var now = Date.now();
-    return Math.floor((Date.UTC(departure.getFullYear(), departure.getMonth(), departure.getDate())
-        - now ) /(1000 * 60 * 60 * 24));
+    var departure = new Date('August 11, 2019 12:00:00 GMT+0200');
+    var now = new Date();
+
+    var diff = Date.UTC(departure.getFullYear(),
+                        departure.getMonth(),
+                        departure.getDate(),
+                        departure.getHours(),
+                        departure.getMinutes())
+        - Date.UTC(now.getFullYear(),
+                   now.getMonth(),
+                   now.getDate(),
+                   now.getHours(),
+                   now.getMinutes())
+    var diff_days = Math.floor(diff /(1000 * 60 * 60 * 24))
+	console.log(diff - diff_days * (1000 * 60 * 60 * 24))
+    var diff_hours = Math.floor((diff - diff_days * (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    return [diff_days, diff_hours];
 }
